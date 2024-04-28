a70d47329fdbd031f1253b834018179bc6e0c53d
Gorcica <n1sh1m4k1@gmail.com>
2024-04-28 15:45:28 +0900

    publicをdist/publicにコピーする設定に変更

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
       'process.env': JSON.stringify(process.env),
     }),
     new CopyPlugin({
-      patterns: [{ from: 'public', to: '' }],
+      patterns: [{ from: 'public', to: 'public' }],
     }),
   ],
 };
